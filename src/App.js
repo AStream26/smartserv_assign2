323c6b6915c71aa86c0733fb8a0bf2fbfdede363
AStream26 <kumaravi113@gmail.com>
2021-12-17 07:56:36 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Fields from "./Component/Fields/Fields";
+import Format from "./Component/Format/Format";
+import Input from "./Component/Input/input";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col, Container } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  let [files, setFiles] = useState([]);
+  let [field, setField] = useState([]);
+  console.log(field);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h5>Import Products</h5>
+      <Row>
+        <Col xs={12} md={6}>
+          <Input files={files} setFiles={setFiles} setField={setField} />
+        </Col>
+        <Col xs={12} md={6}>
+          <Format />
+        </Col>
+
+        <Col xs={10}>
+          <Fields fields={field} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/Component/Fields/Fields.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Item from "./Item";
+import { Form, Row, Col } from "react-bootstrap";
+const Fields = ({ fields }) => {
+  let [selected, setSelected] = useState([]);
+  let [Final, SetFinal] = useState(false);
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        margin: "2em",
+        border: "1px solid black",
+        padding: "1em",
+        boxShadow: "2px 1px 10px grey",
+      }}
+    >
+      <span>Display Handling</span>
+      <br /> <br />
+      Select the fields to be displayed
+      <Row>
+        <Col xs={12} md={3}>
+          Available Fields
+        </Col>
+        <Col xs={2}></Col>
+        <Col md={3}>Fields to be Displayed</Col>
+      </Row>
+      <Row>
+        <Col md={3} style={{ border: "1px solid grey" }}>
+          {fields.map((el, index) => (
+            <Item
+              key={index}
+              item={el.value}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          ))}
+        </Col>
+        <Col className="m-2 p-5" xs={2}>
+          <button onClick={() => SetFinal(true)}>>></button>
+          <br />
+          <br />
+          <button>>></button>
+        </Col>
+        <Col md={3} style={{ border: "1px solid grey" }}>
+          {Final ? (
+            <>
+              {selected.map((el, index) => (
+                <Item
+                  key={index}
+                  item={el}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+              ))}
+            </>
+          ) : (
+            <></>
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Fields;
