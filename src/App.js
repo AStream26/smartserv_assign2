f1ad7fe3a56c258e96afab88bddddb09e52ff2d5
AStream26 <kumaravi113@gmail.com>
2021-12-17 08:50:04 +0530

    dep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Fields from "./Component/Fields/Fields";
-import Format from "./Component/Format/Format";
-import Input from "./Component/Input/input";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Row, Col, Container } from "react-bootstrap";
 import { useState } from "react";
+import Home from "./Component/Home";
+import { Route, Switch } from "react-router-dom";
+import Display from "./Component/Display";
 
 function App() {
   let [files, setFiles] = useState([]);
@@ -11,19 +11,33 @@ function App() {
   console.log(field);
   return (
     <Container>
-      <h5>Import Products</h5>
-      <Row>
-        <Col xs={12} md={6}>
-          <Input files={files} setFiles={setFiles} setField={setField} />
-        </Col>
-        <Col xs={12} md={6}>
-          <Format />
-        </Col>
-
-        <Col xs={10}>
-          <Fields fields={field} />
-        </Col>
-      </Row>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Home
+              {...props}
+              files={files}
+              field={field}
+              setField={setField}
+              setFiles={setFiles}
+            />
+          )}
+        />
+        <Route
+          path="/display"
+          render={(props) => (
+            <Display
+              {...props}
+              files={files}
+              field={field}
+              setField={setField}
+              setFiles={setFiles}
+            />
+          )}
+        />
+      </Switch>
     </Container>
   );
 }
--- src/Component/Fields/Fields.js
+++ src/Component/Fields/Fields.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import Item from "./Item";
 import { Form, Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 const Fields = ({ fields }) => {
   let [selected, setSelected] = useState([]);
   let [Final, SetFinal] = useState(false);
+  let history = useHistory();
+  let show = () => {
+    history.push("/display");
+  };
   return (
     <div
       style={{
@@ -36,10 +41,10 @@ const Fields = ({ fields }) => {
           ))}
         </Col>
         <Col className="m-2 p-5" xs={2}>
-          <button onClick={() => SetFinal(true)}>>></button>
+          <button onClick={() => SetFinal(true)}>{">>"}</button>
           <br />
           <br />
-          <button>>></button>
+          <button>{"<<"}</button>
         </Col>
         <Col md={3} style={{ border: "1px solid grey" }}>
           {Final ? (
@@ -58,6 +63,29 @@ const Fields = ({ fields }) => {
           )}
         </Col>
       </Row>
+      {Final ? (
+        <>
+          <Row className="m-2">
+            <Col md={8}></Col>
+            <Col md={4}>
+              <button
+                onClick={show}
+                style={{
+                  backgroundColor: "green",
+                  color: "white",
+                  fontSize: "500",
+                }}
+              >
+                {" "}
+                Next
+              </button>
+              <span style={{ color: "red", margin: "1em" }}>cancel</span>
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
